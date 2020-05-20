3a98569b7661a319313551bef43dfc8f4c737583
imac1080 <imac1080@gmail.com>
2020-05-20 17:50:41 +0200

    ranking and update

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -39,5 +39,27 @@ module.exports = function(app, db) {
             }
         })
     });
+
+    app.post("/GetRankingUser",  async (req, res) => {
+        console.log(req.body.username);
+        db.collection('RankingCollection').findOne({ username: req.body.username}).then(result => {
+            res.send(result);
+        })
+    });
+
+    app.post("/UpdateRankingUser",  async (req, res) => {
+        console.log(req.body.username);
+        db.collection('RankingCollection').update({ username: req.body.username}, {$set:{ranking:req.body.ranking}}).then(result => {            
+            res.send("update ok");
+        })
+    });
+
+    app.get("/Ranking",  async (req, res) => {        
+        var mysort = { ranking: -1 };
+        db.collection('RankingCollection').find({}).sort(mysort).toArray(function(err, result) {
+            if (err) throw err;
+            res.send(result);
+          });
+    });
     
 }
