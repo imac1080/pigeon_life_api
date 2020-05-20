ad608aad13beaa1dd291fb38673c85296842f4f9
imac1080 <imac1080@gmail.com>
2020-05-20 16:52:56 +0200

    login

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,17 +1,7 @@
  var ObjectID = require('mongodb').ObjectID
 
 module.exports = function(app, db) {
-    app.get('/notes/:id', (req, res) => {
-		const id = req.params.id;
-		const details = {'_id': new ObjectID(id) };
-		db.collection('usuarios').findOne(details, (err, item) => {
-			if (err) {
-				res.send({ 'error': 'An error has occured' });
-			} else {
-				res.send(item);
-			}
-		});
-    });
+    
     
     app.post("/newUser", async (req, res) => {        
 		const user = {'username': req.body.username,'password':req.body.password };
@@ -31,6 +21,17 @@ module.exports = function(app, db) {
                 res.send(true);
             }else{                
                 res.send(false);
+            }
+        })
+    });
+
+    app.post("/LoginUser",  async (req, res) => {
+        console.log(req.body.username);
+        db.collection('usuarios').findOne({ username: req.body.username}).then(result => {
+            if(result!=null){
+                res.send(result.password);
+            }else{
+                res.send(null);
             }
         })
     });
